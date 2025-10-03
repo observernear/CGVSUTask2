cfebf42d86ac8dc6dbf19c8fa35e9aece3eee3a4
observernear <lapinikita@mail.ru>
2025-10-03 23:49:13 +0300

    changed window title

--- src/main/cgvsu/com/Main.java
+++ src/main/cgvsu/com/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainwindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Proto Curve App");
+        stage.setTitle("Bezier Curve App");
         stage.setScene(scene);
         stage.show();
     }
